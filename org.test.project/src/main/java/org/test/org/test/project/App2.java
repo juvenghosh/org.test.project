76b8aee81d5aba218bb3bfdf65d624874c704512
juvenghosh <juven.ghosh@gmail.com>
2020-09-15 09:58:23 +0530

    Added a new java class

--- org.test.project/src/main/java/org/test/org/test/project/App2.java
+++ org.test.project/src/main/java/org/test/org/test/project/App2.java
@@ -4,11 +4,11 @@ package org.test.org.test.project;
  * Hello world!
  *
  */
-public class App 
+public class App2 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!3" );
-        System.out.println( "Hello World!4" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!2" );
     }
 }
